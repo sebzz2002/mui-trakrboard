48d06e140b658538fc8193e4133dfd736510f694
Sebin CM <sebinsebzz2002@gmail.com>
2023-07-12 16:14:47 +0530

    random

---
+++ packages/app/features/login/screen.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Slider from '@mui/material/Slider'
+
+export function LoginScreen() {
+  return (
+    <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
+  )
+}
